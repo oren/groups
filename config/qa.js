6f020b59bbacfb0aa3e9d3bd6bf41dd51bfed3c8
Oren <ogolan@yp.com>
2013-05-12 14:36:47 -0700

    port

--- config/qa.js
+++ config/qa.js
@@ -1,11 +1,14 @@
 "use strict";
 
+var path = require('path');
+
 // var ejs = require('ejs');
 
 var config = {
   port: 7000,
   // engine: ejs,
-  templates: './templates'
+  templates: './templates',
+  db: require('../db')(path.join(__dirname, '..', 'groupsDB'))
 }
 
 module.exports = config;
