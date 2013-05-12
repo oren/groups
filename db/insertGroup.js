d52b6e9b41f578d1e48b6bb8ec4e401ad639c104
Oren <ogolan@yp.com>
2013-05-12 15:06:35 -0700

    permalinks

--- db/insertGroup.js
+++ db/insertGroup.js
@@ -5,10 +5,15 @@ var timestamp = require('monotonic-timestamp')
 
 var now = null;
 
+// http://stackoverflow.com/questions/2519818/create-a-permalink-with-javascript
+function doDashes(str) {
+  return str.replace(/[^a-z0-9]+/gi, '-').replace(/^-*|-*$/g, '').toLowerCase();
+};
+
 module.exports = function (db, subs) {
   return function(name, title, user, cb){
     now = timestamp();
-    subs.groups.put(now, {name: name, title: title, user: user}, function (err) {
+    subs.groups.put(now, {name: name, permalink: doDashes(name), title: title, user: user}, function (err) {
       if (err) {return cb(err) };
       
       cb(null, now)
