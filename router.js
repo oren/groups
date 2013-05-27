3e1015379e4412c2be3a028046d8b071d8955486
Oren <ogolan@yp.com>
2013-05-26 20:02:32 -0700

    test for assets

--- router.js
+++ router.js
@@ -1,12 +1,25 @@
 'use strict';
 
 // npm packages
+var path = require('path');
 // var router = require('routes')();
 
+
 module.exports = function(url) {
-  if (url === '/') {
-    return 'root';
-  }
+  if (url === '/') { return 'root' };
+
+  var isStatic = path.extname(url) === '.css' || 
+    path.extname(url) === '.jpg' || 
+    path.extname(url) === '.JPG' || 
+    path.extname(url) === '.png' || 
+    path.extname(url) === '.PNG' || 
+    path.extname(url) === '.js' || 
+    path.extname(url) === '.ico' || 
+    path.extname(url) === '.html';
+
+  if (isStatic) { return 'static' };
+
+  return null;
 };
 
 // router.addRoute('/:group', require('./routes/group.js'));
--- test/router.js
+++ test/router.js
@@ -9,12 +9,12 @@ var router = require('../router.js');
 test('router', function(t) {
   // t.plan(1);
 
-  // t.equal(router('/1.jpg'), 'static'); 
-  // t.equal(router('/1.JPG'), 'static'); 
-  // t.equal(router('/1.png'), 'static'); 
-  // t.equal(router('/1.PNG'), 'static'); 
-  // t.equal(router('/1.css'), 'static'); 
-  // t.equal(router('/1.js'), 'static'); 
+  t.equal(router('/1.jpg'), 'static'); 
+  t.equal(router('/1.JPG'), 'static'); 
+  t.equal(router('/1.png'), 'static'); 
+  t.equal(router('/1.PNG'), 'static'); 
+  t.equal(router('/1.css'), 'static'); 
+  t.equal(router('/1.js'), 'static'); 
   t.equal(router('/'), 'root'); 
   // t.equal(router('/cats'), 'group'); 
   // t.equal(router('/cats/?format=application/json'), 'group'); 
